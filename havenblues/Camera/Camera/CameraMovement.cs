ee4419411fbf44494eeb7fb51176de1d3348de52
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2022-11-27 20:52:15 -0800

    Update CameraMovement.cs

--- havenblues/Camera/Camera/CameraMovement.cs
+++ havenblues/Camera/Camera/CameraMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
 
+   //Changed camera system to the built in Cinematic Unity system
 
     public Vector2 maxPos;
     public Vector2 minPos;
